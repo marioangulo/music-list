f9b3dbc4a7583c808b16027cc34adc75f38da540
marioangulo <mario.e.angulo@gmail.com>
2012-11-15 17:17:45 -0700

    First commit for the project

---
+++ directory.py
@@ -0,0 +1,28 @@
+##
+# ver 1.0
+# Project Description:
+# This is just the start of my project im going to implement using python as a backend to do the functionality 
+# then I will implement some type of front end which lets you select and deselect what you want to save to a file.
+##
+import os
+
+def getDirectoryStructure(path):
+	i = 0
+	fout = open("musiclist.txt", "w")
+	for (path, dirs, files) in os.walk(path):
+
+	   	fout.write("--------------------------\n")
+	    fout.write(path + "\n")
+	    for d in dirs:
+	    	fout.write("\t" + d + "\n")
+		for f in files:
+			fout.write("\t\t" + f + "\n")
+	    fout.write("--------------------------\n")
+	fout.close()
+	print "Finished!"
+
+
+print "Input your music directory path >"
+path = raw_input();
+
+getDirectoryStructure(path);
